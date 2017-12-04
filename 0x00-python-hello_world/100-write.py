cb2b405926def2c06efe756847ce15da91e6cf6f
Steven Garcia <steven.garcia@holbertonschool.com>
2017-12-04 15:06:57 -0800

    testing

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-import sys
-sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+from sys import stderr
+stderr("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
