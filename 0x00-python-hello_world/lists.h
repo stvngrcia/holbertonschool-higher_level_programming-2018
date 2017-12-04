8b08a0ad3be6670b7a16bf2b80f4ee168c4acc91
Steven Garcia <steven.garcia@holbertonschool.com>
2017-12-04 12:00:57 -0800

    Adding header files

---
+++ 0x00-python-hello_world/lists.h
@@ -0,0 +1,27 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
+void free_listint(listint_t *head);
+int check_cycle(listint_t *list);
+size_t *add_to_array(size_t **, int);
+int check_address(size_t *, listint_t *, int);
+
+#endif /* LISTS_H */
