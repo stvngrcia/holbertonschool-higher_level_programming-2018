de6d2daca4fcaeebb646b0fb883652b3501e7db9
Steven Garcia <stevengarciaacuna@gmail.com>
2017-12-05 18:20:06 -0800

    Update 5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,4 +2,4 @@
 for i in range(0, 99):
     if i < 9:
         print(0, end="")
-    print("{}".format(i), end=", " if i < 98 else '{}\n'.format(", 99"))
+    print("{}".format(i), end=", " if i < 98 else ', {}\n'.format(99))
