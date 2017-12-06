d2b6ba52a03f016c8364a42d4025167c44ee02d4
Steven Garcia <steven.garcia@holbertonschool.com>
2017-12-05 21:13:40 -0800

    fixing pep8 syntax

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -10,6 +10,6 @@ if (number % 10 > 5):
 elif (number % 10 == 0):
     print("{} is {} {}".format(number, number % 10, zero))
 elif (number < 0):
-	print("{} is -{} {}".format(number, (number * -1) % 10, less_six))
+    print("{} is -{} {}".format(number, (number * -1) % 10, less_six))
 else:
     print("{} is {} {}".format(number, number % 10, less_six))
