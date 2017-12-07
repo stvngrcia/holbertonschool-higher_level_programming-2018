f0a1aad2ca7b3a926f1893980bc79b54f6c4a15b
stvngrcia <stevengarciaacuna@gmail.com>
2017-12-07 13:08:06 -0800

    calls a function determined by the parameter enter:

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+from sys import argv
+
+
+def my_calc():
+    operations = {"+": add, "-": sub, "*": mul, "/": div}
+    length = len(argv) - 1
+    if (length < 3):
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    a = int(argv[1])
+    b = int(argv[3])
+    operator = argv[2]
+
+    if operator not in operations:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    result = operations[operator](a, b)
+    print("{} {} {} = {}".format(a, operator, b, result))
+
+
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    my_calc()
