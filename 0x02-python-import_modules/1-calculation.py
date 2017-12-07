e171f3f8dfba30f51cac9db0f20bb1b7a0cbaba8
stvngrcia <stevengarciaacuna@gmail.com>
2017-12-07 12:33:03 -0800

    moving import

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
+
 
 if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
     print("{} + {} = {}".format(a, b, add(a, b)))
