8a8d932145095b9626fde7bf2be7315fa4caa817
Steven Garcia <steven.garcia@holbertonschool.com>
2017-12-07 11:39:08 -0800

    prints the number of arguments followed by a list of arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+from sys import argv
+
+def arguments():
+    arg_string = "arguments"
+    c_or_d = "."
+    length = len(argv) - 1
+    if (length == 1):
+        arg_string = "argument"
+    if (length -1 >= 0):
+        c_or_d = ":"
+
+    print("{} {}{}".format(length, arg_string, c_or_d,))
+
+    for index, arg in enumerate(argv):
+        if (index > 0):
+            print("{}: {}".format(index, arg))
+
+if __name__ == "__main__":
+    arguments()
