331e5965eaa06588f95de9194bf1649a82bba991
stvngrcia <steven.garcia@holbertonschool.com>
2017-12-07 11:50:08 -0800

    Adds an prints an infinate amount of numbers passed as arguments to the program

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,15 @@
+#!/urs/bin/python3
+from sys import argv
+
+
+def inf_add():
+    sum = 0
+
+    for index, num in enumerate(argv):
+        if (index > 0):
+            sum = sum + int(num)
+    print("{:d}".format(sum))
+
+
+if __name__ == "__main__":
+    inf_add()
