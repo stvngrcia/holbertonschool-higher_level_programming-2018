3e976c885e9183e7e384f3c0403ae22b971303cb
stvngrcia <stevengarciaacuna@gmail.com>
2017-12-12 12:53:45 -0800

    adding integer flag

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for index, i in enumerate(row):
-            print("{}".format(i), end="")
+            print("{:d}".format(i), end="")
             if (index < len(row) - 1):
                 print("{}".format(" "), end="")
         print()
