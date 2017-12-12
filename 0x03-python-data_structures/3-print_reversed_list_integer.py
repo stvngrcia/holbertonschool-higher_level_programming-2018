bef6fa0ab5f1b27b4104a34947f61bb40b539792
stvngrcia <stevengarciaacuna@gmail.com>
2017-12-12 12:46:09 -0800

    adding integer flag

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,3 +3,5 @@ def print_reversed_list_integer(my_list=[]):
     my_list.reverse()
     for i in my_list:
         print("{:d}".format(i))
+if __name__ == "__main__":
+	print_reversed_list_integer()
