af08ad0f544ab27a1e95c561078834228180c3c8
stvngrcia <stevengarciaacuna@gmail.com>
2017-12-14 12:51:22 -0800

    converts a roman number into an integer

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    number = 0
+    for i, char in enumerate(roman_string):
+        tmp_num = roman[char]
+        try:
+            if tmp_num < roman[roman_string[i + 1]]:
+                tmp_num = tmp_num * -1
+        except IndexError:
+            pass
+        number = number + tmp_num
+    return (number)
