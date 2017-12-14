d1c634a03b0067d52f7ff3c37e1a9bad46e5770b
stvngrcia <stevengarciaacuna@gmail.com>
2017-12-14 13:38:06 -0800

    getting a weighted average

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if len(my_list) == 0:
+        return (0)
+    sum = 0
+    div = 0
+    for item in my_list:
+        sum = sum + (item[0] * item[1])
+        div = div + item[-1]
+    print(sum/div)
+    return (0)
