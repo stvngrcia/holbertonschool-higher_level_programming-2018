689941e34d6563f665f4ac352af7402282924426
stvngrcia <stevengarciaacuna@gmail.com>
2017-12-14 09:57:18 -0800

    sorting a dictionary by its key

---
+++ 0x04-python-more_data_structur/test_files/6-main.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+print_sorted_dictionary = __import__('6-print_sorted_dictionary').print_sorted_dictionary
+
+my_dict = { 'language': "C", 'Number': 89, 'track': "Low level", 'ids': [1, 2, 3] }
+print_sorted_dictionary(my_dict)
