a0c8a2715547909d0913df456831e680bfafe1c0
stvngrcia <stevengarciaacuna@gmail.com>
2017-12-14 12:54:35 -0800

    handling none and not a string

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,6 +2,8 @@
 def roman_to_int(roman_string):
     roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     number = 0
+    if (type(roman_string) is not str or roman_string is None):
+        return (0)
     for i, char in enumerate(roman_string):
         tmp_num = roman[char]
         try:
