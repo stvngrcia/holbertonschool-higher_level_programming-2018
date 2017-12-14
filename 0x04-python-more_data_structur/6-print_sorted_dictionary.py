3d958af19441c7d0595b66f9997beee91d83ea3e
stvngrcia <stevengarciaacuna@gmail.com>
2017-12-14 10:05:15 -0800

    only sorting the list if the dict in not None

--- 0x04-python-more_data_structur/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structur/6-print_sorted_dictionary.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(my_dict):
     keys = []
-    for k, i in my_dict.items():
-        keys.append(k)
+    if my_dict:
+        for k, i in my_dict.items():
+            keys.append(k)
 
-    keys.sort()
-    for k in keys:
-        print("{}: {}".format(k, my_dict[k]))
+        keys.sort()
+        for k in keys:
+            print("{}: {}".format(k, my_dict[k]))
