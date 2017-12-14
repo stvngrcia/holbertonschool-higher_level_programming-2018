9da803358aa8ea960d7b88b626ba37d596b09dd6
stvngrcia <stevengarciaacuna@gmail.com>
2017-12-14 12:28:29 -0800

    returning an empty matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = []
-
     if (matrix and matrix[0] and matrix[0][0]):
+        new_matrix = []
         for row in matrix:
             new_matrix.append(list(map(lambda x: x * x, row)))
-    return (new_matrix)
+        return (new_matrix)
