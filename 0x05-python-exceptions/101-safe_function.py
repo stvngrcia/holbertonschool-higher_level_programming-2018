7b7838ec45f0937a269031f99f44b881fedf2bf4
stvngrcia <stevengarciaacuna@gmail.com>
2018-01-02 12:08:39 -0800

    prints the result of a function

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import sys
+def safe_function(fct, *args):
+    try:
+        return fct(args[0], args[1])
+    except(IndexError, ZeroDivisionError, ValueError) as err:
+        sys.stderr.write("Exception: " + str(err) + "\n")
+        return None
