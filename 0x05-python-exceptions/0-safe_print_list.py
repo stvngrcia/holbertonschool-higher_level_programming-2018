2de61b1b65d85f4674a5c12a4b360f1acc14c3b9
stvngrcia <stevengarciaacuna@gmail.com>
2018-01-02 10:15:54 -0800

    prints a list of items

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,10 @@
+def safe_print_list(my_list=[], x=0):
+    num = 0
+    try:
+        for item in range(0, x):
+            print("{}".format(my_list[item]), end="")
+            num += 1
+    except IndexError:
+        pass
+    print()
+    return num
