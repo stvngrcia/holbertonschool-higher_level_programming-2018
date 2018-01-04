eceec159e7b196cc16bfba237c8841d80befaae8
stvngrcia <stevengarciaacuna@gmail.com>
2018-01-03 16:03:13 -0800

    adding check for tuple typle and len of the tuple

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -13,6 +13,9 @@ class Square():
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
+
+        if not isinstance(position, tuple) and len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
         self.__size = size
         self.__position = position
 
