af7fd86052944600d69ea1b3cfe4724c4f8306c5
stvngrcia <stevengarciaacuna@gmail.com>
2018-01-05 10:06:16 -0800

    adds two integers or float and returns an int

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''
+   Adds two integers
+   a (int | float)
+   b (int | float)
+'''
+def add_integer(a, b):
+    '''
+        Adds two integers
+    '''
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError("a must be an integer")
+
+    if not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError("b must be an integer")
+
+    return int(a + b)
