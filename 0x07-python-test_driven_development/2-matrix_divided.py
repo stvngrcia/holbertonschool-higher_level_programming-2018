d392c4153a8c0e4c70e7b696c6f457cf800a0418
stvngrcia <stevengarciaacuna@gmail.com>
2018-01-05 11:54:15 -0800

    divinging items in matrix

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+def matrix_divided(matrix, div):
+    new_matrix = []
+    try:
+        length  = len(matrix[0])
+    except:
+        pass
+    int_err = "matrix must be a matrix(list of lists) of integers/floats"
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    for row in matrix:
+        new_row = []
+        if not isinstance(row, list):
+            raise TypeError(int_err)
+        if length != len(row):
+            raise TypeError("Each row of the matrix must have the same size")
+        for l in row:
+            if not isinstance(l, (int, float)):
+                raise TypeError(int_err)
+            result = round(l / div, 2)
+            new_row.append(result)
+        new_matrix.append(new_row)
+    return new_matrix
