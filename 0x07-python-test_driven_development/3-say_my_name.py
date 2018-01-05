d26ce4ce0539b3d16e2272968d3249234d847975
stvngrcia <stevengarciaacuna@gmail.com>
2018-01-05 13:45:43 -0800

    adding test for say my name

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''
+    Prints My name is first_name last_name
+    first_name (str)
+    last_name (str)
+'''
+
+def say_my_name(first_name, last_name=""):
+    '''
+        prints name
+    '''
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print("My npame is {} {}".format(first_name, last_name))
