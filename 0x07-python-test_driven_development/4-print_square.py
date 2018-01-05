9f5e71264901dbcdf6d373b796f91df82624ea61
stvngrcia <stevengarciaacuna@gmail.com>
2018-01-05 14:15:52 -0800

    printing a square an adding tests

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''
+    prints a square
+    takes in the size
+'''
+
+
+def print_square(size):
+    square = ""
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for c in range(size):
+        square += "#" * size + "\n"
+    print (square, end="")
