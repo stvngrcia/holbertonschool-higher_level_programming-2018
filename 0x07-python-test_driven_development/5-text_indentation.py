2364634aab15e2c66432a698096e631fe5303006
stvngrcia <stevengarciaacuna@gmail.com>
2018-01-05 15:40:54 -0800

    removing all the spaces at the beginning of the string

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
 def text_indentation(text):
     buf = ""
-    flag = 0
     if not isinstance(text, str):
         raise TypeError("text must be a string")
     for letter in text:
         buf += letter
         if letter == "." or letter == "?" or letter == ":":
             while buf[0] == " ":
-                buf = buf[:2]
-                print(buf[0])
+                buf = buf[1:]
             print(buf)
             print()
             buf = ""
-            flag = 1
