2b41c2401786fe512ecece2d59f57da49a6bf489
stvngrcia <stevengarciaacuna@gmail.com>
2018-01-06 09:50:05 -0800

    adding test for finding the maximum integer in a list

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+    def test_none_value(self):
+        '''
+            Testing when the paratemerter None is passed
+        '''
+        with self.assertRaises(TypeError):
+            max_integer(None)
+
+    def test_right_output(self):
+        '''
+            Testing when passing and argument that is expected to succeed
+        '''
+        self.assertEqual(max_integer([1, 6, 100, 4, 0, -1, 10]), 100)
+
+    def test_no_list(self):
+        '''
+            Testing a parameter that is not a list
+        '''
+        with self.assertRaises(TypeError):
+            max_integer("Not a list")
+
+    def test_empty_list(self):
+        '''
+            Testing an empty list parameter
+        '''
+        self.assertIsNone(max_integer([]))
+
+    def test_no_param(self):
+        '''
+            Testing no parameter
+        '''
+        self.assertIsNone(max_integer())
+
+    def test_equal_values(self):
+        '''
+            Testing with all the list values equal to each other
+        '''
+        self.assertEqual(max_integer([1, 1, 1, 1]), 1)
+
+    def test_no_ints(self):
+        '''
+            Testing with values in the list that are not integers
+        '''
+        with self.assertRaises(TypeError):
+            max_integer([1, "hello", 2, 3])
+
+    def test_floats(self):
+        '''
+            Testing with values in the list that are floats
+        '''
+        with self.assertRaises(TypeError):
+            max_integer([1, 5.0, 2, 3])
+
+    def test_only_negatives(self):
+        '''
+            Testing with negative numbers
+        '''
+        self.assertEqual(max_integer([-10, -100, -6, -3, -1]), -1)
