09c364fc6c44bfa1be6b99b3c311f5c81df58f65
stvngrcia <stevengarciaacuna@gmail.com>
2018-01-08 10:57:31 -0800

    fixing pep8 error

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -6,6 +6,7 @@
 
 class Rectangle:
     number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         '''
             Calling the setters to initialize the instance variables
--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -7,6 +7,7 @@
 class Rectangle:
     number_of_instances = 0
     print_symbol = "#"
+
     def __init__(self, width=0, height=0):
         '''
             Calling the setters to initialize the instance variables
