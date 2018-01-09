a55ba8931b2401e2c43fd2b1d3f37b9c4ad2d261
stvngrcia <stevengarciaacuna@gmail.com>
2018-01-09 15:47:37 -0800

    testing

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -12,8 +12,8 @@ class Rectangle:
         '''
             Calling the setters to initialize the instance variables
         '''
-        self.height = height
         self.width = width
+        self.height = height
         Rectangle.number_of_instances += 1
 
     @property
