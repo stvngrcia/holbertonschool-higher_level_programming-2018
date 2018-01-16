836218db504ae6cf3b4e0b1da478b73719f50b94
stvngrcia <stevengarciaacuna@gmail.com>
2018-01-16 13:41:23 -0800

    adding a class rectangle and checking for the right type on validations

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -19,3 +19,11 @@ class BaseGeometry:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
+
+class Rectangle(BaseGeometry):
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+        self.__width = width
+        self.__height = height
