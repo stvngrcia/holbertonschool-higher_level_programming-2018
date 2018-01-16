79b5868c401cea25b06b77cbd7cc5b9454f02491
stvngrcia <stevengarciaacuna@gmail.com>
2018-01-16 11:10:04 -0800

    checking if an object is a specific class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def is_same_class(obj, a_class):
+    '''
+        Check if the objects have the same class
+    '''
+    if type(obj) is a_class:
+        return True
+    return False
