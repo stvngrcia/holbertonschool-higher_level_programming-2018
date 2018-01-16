77fdbaaae5ecd5ed9a7fcd6535a3b9be33a88034
stvngrcia <stevengarciaacuna@gmail.com>
2018-01-16 12:59:31 -0800

    adding an integer validator function

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''
+    Implementing a Geometry class
+'''
+
+
+class BaseGeometry:
+    def area(self):
+        '''
+            Calculating the area
+        '''
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        '''
+            Validating the integer
+        '''
+        if not isinstance(value, int):
+            raise TypeError("{} must be an  integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
