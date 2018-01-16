90abe9c53901500fcc904b884f4396e99fdfb687
stvngrcia <stevengarciaacuna@gmail.com>
2018-01-16 15:18:03 -0800

    overwritting not equal and equal functions

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''
+    Class that takes an integers
+'''
+class MyInt:
+    def __init__(self, number):
+        self.number = number
+    def __ne__(self, val):
+        return (self.number == val)
+
+    def __eq__(self, val):
+        return (self.number != val)
+
+    def __str__(self):
+        return (str(self.number))
--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -36,7 +36,8 @@ class Rectangle(BaseGeometry):
         return (self.__width * self.__height)
 
     def __str__(self):
-        return ("[{}] {}/{}".format(type(self).__name__, self.__width, self.__height))
+        return ("[{}] {}/{}".format(type(self).__name__,
+                                    self.__width, self.__height))
 
 
 class Square(Rectangle):
