2d238ec95fadf95547a9d26dd8a7e550ba10fa08
stvngrcia <stevengarciaacuna@gmail.com>
2018-01-16 13:53:42 -0800

    implementing area and str. Fixing pep8 errors

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -31,3 +31,10 @@ class Rectangle(BaseGeometry):
 
         self.__width = width
         self.__height = height
+
+    def area(self):
+        return (self.__width * self.__height)
+
+    def __str__(self):
+        return ("[{}] {}/{}".format(type(self).__name__,
+                                    self.__width, self.__height))
