97dacfa81d782dd187de06c62eaea3799509537e
stvngrcia <stevengarciaacuna@gmail.com>
2018-01-17 15:08:24 -0800

    Returns a matrix with integers representing the pascal triangle of n

---
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+
+def pascal_triangle(n):
+    '''
+        Creates a pascal triangle
+        Returns: A matrix containing intergers representing
+                 the pascal triangle of n
+    '''
+    matrix = []
+
+    for row in range(n):
+        tmp_list = [0] * (row + 1)
+        tmp_list[0] = 1
+        tmp_list[row] = 1
+        if row > 1:
+            tmp_list[1] = row
+            tmp_list[row - 1] = row
+        # Getting the position where 0 still exist
+        while 0 in tmp_list:
+            idx = tmp_list.index(0)
+            num = matrix[row - 1][idx] + matrix[row - 1][idx - 1]
+            tmp_list[idx] = num
+
+        matrix.append(tmp_list)
+    return matrix
