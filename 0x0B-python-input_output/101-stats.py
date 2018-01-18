67aa6985cb607c11234d5d4742f22291857c0d4b
stvngrcia <stevengarciaacuna@gmail.com>
2018-01-18 14:37:04 -0800

    spliting the line instead of looping through the dict

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -7,34 +7,29 @@ def print_status():
         Printing the status of the request
     '''
     counter = 0
+    size = 0
     file_size = 0
     status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
                     "403": 0, "404": 0, "405": 0, "500": 0}
 
     for l in sys.stdin:
-        try:
-            counter += 1
-            for code, val in status_codes.items():
-                if (code in l):
-                    status_codes[code] = val + 1
-            my_list = l.split()
-            file_size += int(my_list[-1])
-            if counter == 10:
-                print("File size: {:d}".format(file_size))
-                for code, val in sorted(status_codes.items()):
-                    if (val != 0):
-                        print("{}: {}".format(code, val))
-                        counter = 0
-        except KeyboardInterrupt:
-            print("File size: {:d}".format(file_size))
-            for code, val in status_codes.items():
-                    if (val != 0):
-                        print("{}: {}".format(code, val))
+        line = l.split()
+        size += int(line[-1])
+        code = line[-2]
+        status_codes[code] += 1
+
+        if counter == 9:
+            print("File size: {}".format(size))
+            for key, val in status_codes.items():
+                if (val != 0):
+                    print("{}: {}".format(key, val))
+            counter = 0
+        counter += 1
     if counter < 9:
-            print("File size: {:d}".format(file_size))
-            for code, val in status_codes.items():
-                    if (val != 0):
-                        print("{}: {}".format(code, val))
+        print("File size: {}".format(size))
+        for key, val in status_codes.items():
+            if (val != 0):
+                print("{}: {}".format(key, val))
 
 
 if __name__ == "__main__":
