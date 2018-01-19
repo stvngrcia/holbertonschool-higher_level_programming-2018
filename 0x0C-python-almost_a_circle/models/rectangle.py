ced85089d8629388ac42dd8abf1a09857a015dcf
stvngrcia <stevengarciaacuna@gmail.com>
2018-01-19 11:11:52 -0800

    adding rectangle class getters and setters

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python3
+from models.base import Base
+
+'''
+    Class Rectangle
+'''
+
+
+class Rectangle(Base):
+    '''
+        Defining the Rectangle class
+        Inherits from:
+            Base
+    '''
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        '''
+            Returning private attribute
+        '''
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        '''
+            Setting private attribute
+        '''
+        self.__width = value
+
+    @property
+    def height(self):
+        '''
+            Returning private attribute
+        '''
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        '''
+            Setting private attribute
+        '''
+        self.__height = value
+
+    @property
+    def x(self):
+        '''
+            Returning private attribute
+        '''
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        '''
+            Setting private attribute
+        '''
+        self.__x = value
+
+    @property
+    def y(self):
+        '''
+            Returning private attribute
+        '''
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        '''
+            Setting private attribute
+        '''
+        self.__y = value
