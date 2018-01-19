b986000f74326a8e83755661089e436eed534a57
stvngrcia <stevengarciaacuna@gmail.com>
2018-01-19 11:10:10 -0800

    adding test cases for rectangle class

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle_model.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+import unittest
+from models.rectangle import Rectangle
+
+'''
+    Runs test cases for the Rectangle module
+'''
+
+
+class test_rectangle(unittest.TestCase):
+    '''
+        Testing rectangle
+    '''
+
+    def setUp(self):
+        '''
+            Initializing instance with width and height
+            parameters
+        '''
+        self.r = Rectangle(5, 10)
+
+    def tearDown(self):
+        '''
+            Deleting created instance
+        '''
+        del self.r
+
+    def test_width(self):
+        '''
+            Testing the Rectangle width getter
+        '''
+        self.assertEqual(5, self.r.width)
+
+    def test_height(self):
+        '''
+            Testing the Rectangle height getter
+        '''
+        self.assertEqual(10, self.r.height)
+
+    def test_x(self):
+        '''
+            Testing Rectangle x getter and setter
+        '''
+
+        self.r.x = 54
+        self.assertEqual(54, self.r.x)
+        self.assertEqual(0, self.r.y)
+
+    def test_y(self):
+        '''
+            Testing Rectangle y getter and setter
+        '''
+
+        self.r.y = 45
+        self.assertEqual(45, self.r.y)
+        self.assertEqual(0, self.r.x)
+
+    def test_arectangle_id(self):
+        '''
+            Test the id for Rectangle
+        '''
+        self.assertEqual(2, self.r.id)
