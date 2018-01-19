8f51bb66ebd96491a88943c6bad818ca1e6cd940
stvngrcia <stevengarciaacuna@gmail.com>
2018-01-19 10:25:43 -0800

    adding base to the models folder to manage the ids of instances across classes

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''
+    Creating the base class of all other classes for this project.
+'''
+class Base:
+    '''
+        This class will manage the id attribute for all the classes.
+        Arguments:
+            @id: The id for a specific instance.
+    '''
+
+    __nb_objects = 0
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
