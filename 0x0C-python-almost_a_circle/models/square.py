a257da7b4a94b83c2dee574906394dc038f1a545
stvngrcia <stevengarciaacuna@gmail.com>
2018-01-20 09:22:11 -0800

    creating a getter and setter for rectanlge

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,11 +8,28 @@ from models.rectangle import Rectangle
 class Square(Rectangle):
     '''
         Inherits from:
-            Rectanle
+            Rectangle
             Base
     '''
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
+
+
+    @property
+    def size(self):
+        '''
+            returning the width of the square though its getter
+        '''
+        return self.width
+
+    @size.setter
+    def size(self, val):
+        '''
+            setting the width and height throught their setters
+        '''
+        self.setter_validation("width", val)
+        self.width = val
+        self.height = val
 
     def __str__(self):
         '''
