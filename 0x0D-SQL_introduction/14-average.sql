af20b80d7ee6538f04a987f5d32ad91a853aa1ba
Steven Garcia <27@p3-4.hbtn.io>
2018-02-26 13:35:54 -0800

    using uppercase

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,2 +1,2 @@
 -- calculates the average of all the score
-SELECT AVG(score) as average FROM second_table;
+SELECT AVG(score) AS average FROM second_table;
--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- List all records with name not null
-SELECT score, name FROM second_table where name IS NOT NULL ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
