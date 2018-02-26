8d252353f8a644f23077a112e53ac057340da8de
Steven Garcia <27@p3-4.hbtn.io>
2018-02-26 13:59:39 -0800

    descending

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,4 +1,2 @@
 -- Lists the number of records with the same score in the table
-SELECT score, COUNT(score) AS number FROM second_table
-GROUP BY score
-ORDER BY number DESC;
+SELECT score, COUNT(score) AS number FROM second_table GROUP BY score ORDER BY number DESC;
