69be32919a7d81f86136f752dcc63b0d8564f913
stvngrcia <stevengarciaacuna@gmail.com>
2018-03-03 20:07:20 -0800

    checking user permissions

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
--- List the privileges of users user_0d_1 and user_0d_2
+-- List the privileges of users user_0d_1 and  user_0d_2
 SHOW GRANTS FOR user_0d_1@localhost;
 SHOW GRANTS FOR user_0d_2@localhost;
