2662713fca9753e9d8f06e0ffa52044473a4be09
stvngrcia <stevengarciaacuna@gmail.com>
2018-03-03 20:38:40 -0800

    creating a table with UNIQUE

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,2 +1,2 @@
 -- creates a table named unique_id
-CREATE TABLE IF NOT EXISTS unique_id(id INT DESFAULT 1 UNIQUE, name VARCHAR(256))
+CREATE TABLE IF NOT EXISTS unique_id(id INT DEFAULT 1 UNIQUE, name VARCHAR(256))
