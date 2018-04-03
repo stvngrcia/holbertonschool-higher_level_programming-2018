1b5363c3124663ec447c260d2c6429d74e4aed4e
stvngrcia <stevengarciaacuna@gmail.com>
2018-04-03 13:45:48 -0700

    testing for no numbers

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,6 +5,8 @@ def find_peak(nums):
     '''
         Finds the pick in a list of numbers
     '''
+    if len(nums) == 0:
+        return None
     if len(nums) == 1:
         return (nums[0])
     length = len(nums) - 1
