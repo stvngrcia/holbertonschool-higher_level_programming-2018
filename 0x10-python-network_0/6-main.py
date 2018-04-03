ea91ba6fb29cc7f3406bb337f77afcfa0ead50b0
stvngrcia <stevengarciaacuna@gmail.com>
2018-04-03 13:05:47 -0700

    Finds the pick in a list of numbers

---
+++ 0x10-python-network_0/6-main.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+""" Test function find_peak """
+find_peak = __import__('6-peak').find_peak
+
+print(find_peak([1, 2, 4, 6, 3]))
+print(find_peak([4, 2, 1, 2, 3, 1]))
--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+
+
+def find_peak(nums):
+    '''
+        Finds the pick in a list of numbers
+    '''
+    length = len(nums)
+    if length < 3:
+        return(sorted(nums[-1]))
+
+    for idx in range(1, length):
+        if idx + 1 < length:
+            if nums[idx + 1] < nums[idx] and nums[idx - 1] < nums[idx]:
+                pick = nums[idx]
+
+    return pick
