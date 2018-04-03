95b1994ec32f67da9a20e2a60ddccb6670b231b3
stvngrcia <stevengarciaacuna@gmail.com>
2018-04-03 15:27:39 -0700

    using a while loop and looking from the end of the list

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -13,7 +13,9 @@ def find_peak(nums):
     if length == 2:
         return nums[0] if nums[0] > nums[1] else nums[1]
 
-    for idx in range(0, length):
+    # for idx in range(0, length):
+    idx = length - 1
+    while (idx >= 0):
         value = nums[idx]
         if (idx > 0 and idx < length - 1 and
                 nums[idx + 1] < value and nums[idx - 1] < value):
@@ -22,4 +24,5 @@ def find_peak(nums):
             return value
         elif idx == length - 1 and nums[idx - 1] < value:
             return value
+        idx = idx - 1
     return pick
