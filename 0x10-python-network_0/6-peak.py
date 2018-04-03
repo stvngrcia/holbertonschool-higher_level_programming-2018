1be1a343868901dcdb93ea0fa62a0baf54a2caae
stvngrcia <stevengarciaacuna@gmail.com>
2018-04-03 13:57:57 -0700

    testing

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,15 +5,20 @@ def find_peak(nums):
     '''
         Finds the pick in a list of numbers
     '''
-    length = len(nums) - 1
-
-    for idx, value in enumerate(nums):
-        if idx > 0 and idx < length:
-            if nums[idx + 1] < value and nums[idx - 1] < value:
-                pick = value
-        else:
-            if idx == 0 and nums[idx + 1] < value:
-                pick = value
-            elif idx == length and nums[idx - 1] < value:
-                pick = value
-    return pick
+    if len(nums) == 0:
+        return None
+    if len(nums) == 1:
+        return nums[0]
+    if len(nums) == 2:
+        return nums[0] if nums[0] > nums[1] else nums[1]
+    res = list()
+    if nums[0] > nums[1]:
+        return nums[0]
+    if nums[-1] > nums[-2]:
+        return nums[len(nums)-1]
+    for i in range(1, len(nums)-1):
+        pre = nums[i-1]
+        cur = nums[i]
+        nex = nums[i+1]
+        if cur > pre and cur > nex:
+            return nums[i]
