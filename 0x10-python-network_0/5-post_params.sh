d3ea129307efe6d4a46ad19187601e6ea3cead96
stvngrcia <stevengarciaacuna@gmail.com>
2018-04-03 10:28:22 -0700

    sends a POST request to the passed URL, and displays the body of the response

---
+++ 0x10-python-network_0/5-post_params.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+# sends a POST request to the passed URL, and displays the body of the response
+curl -X POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
