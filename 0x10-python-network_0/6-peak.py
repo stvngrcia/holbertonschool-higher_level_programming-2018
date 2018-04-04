68d974460815a83f42da9fc3b6ae74a468b5c4da
stvngrcia <stevengarciaacuna@gmail.com>
2018-04-03 17:24:54 -0700

    dealing with equal numbers

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -11,25 +11,15 @@ def find_peak(nums):
     if length == 1:
         return (nums[0])
     if length == 2:
-        return nums[0] if nums[0] > nums[1] else nums[1]
+        return nums[0] if nums[0] >= nums[1] else nums[1]
 
-    mid = length // 2
-    for idx in range(mid, length):
+    for idx in range(0, length):
         value = nums[idx]
         if (idx > 0 and idx < length - 1 and
-                nums[idx + 1] < value and nums[idx - 1] < value):
+                nums[idx + 1] <= value and nums[idx - 1] <= value):
                 return value
-        elif idx == 0 and nums[idx + 1] < value:
+        elif idx == 0 and nums[idx + 1] <= value:
             return value
-        elif idx == length - 1 and nums[idx - 1] < value:
-            return value
-    for idx in range(0, mid):
-        value = nums[idx]
-        if (idx > 0 and idx < length - 1 and
-                nums[idx + 1] < value and nums[idx - 1] < value):
-                return value
-        elif idx == 0 and nums[idx + 1] < value:
-            return value
-        elif idx == length - 1 and nums[idx - 1] < value:
+        elif idx == length - 1 and nums[idx - 1] <= value:
             return value
     return pick
