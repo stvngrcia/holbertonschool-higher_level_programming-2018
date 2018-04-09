23f4d71143d5a8c71c9b9890979b2450253244fc
stvngrcia <stevengarciaacuna@gmail.com>
2018-04-09 13:32:01 -0700

    grabbing the latest commits to a repository

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -2,3 +2,18 @@
 import sys
 import requests
 
+
+if __name__ == "__main__":
+    repo_name = sys.argv[1]
+    owner = sys.argv[2]
+
+    url = "https://api.github.com/repos/{}/{}/commits".format(repo_name, owner)
+    response = requests.get(url)
+    json_response = response.json()
+    counter = 0
+    for commit in json_response:
+        if counter > 9:
+            break
+        user = commit["commit"]["author"]["name"]
+        print("{}: {}".format(commit["sha"], user))
+        counter += 1
