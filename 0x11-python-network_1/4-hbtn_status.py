0ddd60df01ca508e8af9b10e453e7c2fb495e5eb
stvngrcia <stevengarciaacuna@gmail.com>
2018-04-09 11:09:55 -0700

    script that fetches https://intranet.hbtn.io/status

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import requests
+
+if __name__ == "__main__":
+    url = "https://intranet.hbtn.io/status"
+    response = requests.get(url)
+    print("Body response:")
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))
