f6783f132f553fd130ff570a88420767bb4c9d13
stvngrcia <stevengarciaacuna@gmail.com>
2018-04-09 13:53:05 -0700

    testing

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
-import sys
-import requests
 
 
 if __name__ == "__main__":
-    repo_name = sys.argv[1]
-    owner = sys.argv[2]
+    import sys
+    import requests
 
-    url = "https://api.github.com/repos/{}/{}/commits".format(owner, repo_name)
-    response = requests.get(url)
-    data = response.json()
-    data_top = data[:10]
-    for commit in data_top:
-        user = commit.get("commit").get("author").get("name")
-        print("{}: {}".format(commit.get("sha"), user))
+    repo = sys.argv[1]
+    owner = sys.argv[2]
+    url = "https://api.github.com/repos/{}/{}/commits".format(owner, repo)
+    res = requests.get(url)
+    commits = res.json()
+    count = 0
+    for commit in commits:
+        sha = commit.get("sha")
+        name = commit.get("commit").get("author").get("name")
+        print("{}: {}".format(sha, name))
+        count += 1
+        if count == 10:
+            break
