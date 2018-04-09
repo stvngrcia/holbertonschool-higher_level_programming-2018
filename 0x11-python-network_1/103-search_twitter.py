747037b13686ad54f4a40ce92c9943184766dd7b
stvngrcia <stevengarciaacuna@gmail.com>
2018-04-09 15:27:24 -0700

    removing unnecesary  funcntion call

--- 0x11-python-network_1/103-search_twitter.py
+++ 0x11-python-network_1/103-search_twitter.py
@@ -16,7 +16,7 @@ def search_tweets():
     api_key = base64.b64encode(sys.argv[1].encode())
     api_secret = base64.b64encode(sys.argv[2].encode())
 
-    auth = OAuth1(api_key, api_secret)
+    auth = (api_key, api_secret)
     url = 'https://api.twitter.com/1.1/search/tweets.json'
     response = requests.get(url, auth=auth)
     print(response.json())
