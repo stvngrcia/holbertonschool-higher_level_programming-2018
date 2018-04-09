d4eae1be7e4a0172cc637abfb5ff0d602bfd928f
stvngrcia <stevengarciaacuna@gmail.com>
2018-04-09 13:03:39 -0700

    using get to retrive headers

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -5,5 +5,4 @@ import requests
 if __name__ == "__main__":
     url = sys.argv[1]
     response = requests.get(url)
-    headers = response.headers
-    print(headers["X-Request-Id"])
+    print(response.headers.get("X-Request-Id"))
