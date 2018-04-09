8d44254c06876721c2dbd462d00180a6141ab5c8
stvngrcia <stevengarciaacuna@gmail.com>
2018-04-09 10:09:34 -0700

    getting the body response with urllib

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from urllib.request import Request, urlopen
+
+if __name__ == "__main__":
+    url = "https://intranet.hbtn.io/status"
+    req = Request(url)
+    with urlopen(req) as response:
+        page = response.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(page)))
+    print("\t- content: {}".format(page))
+    print("\t- utf8 content: {}".format(page.decode("utf-8")))
