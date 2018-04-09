39be45afcdbd9f6dd64bfb6a1dd4cea62f60ff51
stvngrcia <stevengarciaacuna@gmail.com>
2018-04-09 13:40:18 -0700

    getting only 10 results

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -9,11 +9,8 @@ if __name__ == "__main__":
 
     url = "https://api.github.com/repos/{}/{}/commits".format(repo_name, owner)
     response = requests.get(url)
-    json_response = response.json()
-    counter = 0
-    for commit in json_response:
-        if counter > 9:
-            break
+    data = response.json()
+    data_top = data[:10]
+    for commit in data_top:
         user = commit["commit"]["author"]["name"]
         print("{}: {}".format(commit["sha"], user))
-        counter += 1
