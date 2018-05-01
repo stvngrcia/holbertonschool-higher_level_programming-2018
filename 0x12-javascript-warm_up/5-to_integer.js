ce9da62fc08a2d51310d128a0367a4d8eecc230f
stvngrcia <stevengarciaacuna@gmail.com>
2018-05-01 15:13:12 -0700

    adding string to say is not a number

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,5 +2,5 @@
 if (isNaN(parseInt(process.argv[2]))) {
   console.log('Not a number');
 } else {
-  console.log(parseInt(process.argv[2]));
+  console.log("My number: " + parseInt(process.argv[2]));
 }
