6b0fe9046996fdbc57af10caff49f76cc600ef6f
stvngrcia <stevengarciaacuna@gmail.com>
2018-05-01 18:54:07 -0700

    fixing error for two digits only

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,7 +4,7 @@ function second (myArray) {
   if (myArray.length === 2 || myArray.length === 3) { return (0); }
 
   let max = myArray[2];
-  let secondMax = myArray[2];
+  let secondMax = myArray[3];
 
   for (let i = 2; i < myArray.length; i++) {
     if (myArray[i] > max) {
