7e369b6aa6e0e122cce36c95821c93ece0ee1c6f
stvngrcia <stevengarciaacuna@gmail.com>
2018-05-08 12:21:14 -0700

    fixing semistandard

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -5,6 +5,6 @@ let episode = process.argv[2];
 let url = 'http://swapi.co/api/films/' + episode;
 
 request(url, function (err, data, body) {
-    if (err) throw err;
-    console.log(JSON.parse(body)['title']);
+  if (err) throw err;
+  console.log(JSON.parse(body)['title']);
 });
