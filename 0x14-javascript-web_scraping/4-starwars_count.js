0755eac15880698be8036b0b8b3fb770e96c5f59
stvngrcia <stevengarciaacuna@gmail.com>
2018-05-08 12:45:53 -0700

    checking for undefined url

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,20 +2,22 @@
 let request = require('request');
 let url = process.argv[2];
 let counter = 0;
-request(url, function (err, data, body) {
-  if (err) {
-    console.log(counter);
-    throw err;
-  }
-  if (url === 'http://swapi.co/api/films') {
-    let films = JSON.parse(body).results;
-    for (let result = 0; result < films.length; result++) {
-      for (let char = 0; char < films[result]['characters'].length; char++) {
-        if (films[result]['characters'][char] === 'https://swapi.co/api/people/18/') {
-          counter += 1;
+if (url !== undefined) {
+  request(url, function (err, data, body) {
+    if (err) {
+      console.log(counter);
+      console.log(err);
+    }
+    if (url === 'http://swapi.co/api/films') {
+      let films = JSON.parse(body).results;
+      for (let result = 0; result < films.length; result++) {
+        for (let char = 0; char < films[result]['characters'].length; char++) {
+          if (films[result]['characters'][char] === 'https://swapi.co/api/people/18/') {
+            counter += 1;
+          }
         }
       }
     }
-  }
-  console.log(counter);
-});
+    console.log(counter);
+  });
+}
