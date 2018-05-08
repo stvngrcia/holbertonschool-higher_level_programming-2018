8083f341f8de29e7e9202d3f71385d9518fd277a
stvngrcia <stevengarciaacuna@gmail.com>
2018-05-08 12:59:17 -0700

    changing variable names inorder to have smaller lines

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,19 +1,21 @@
 #!/usr/bin/node
 let request = require('request');
 let url = process.argv[2];
-let counter = 0;
+
 request(url, function (err, data, body) {
   if (err) {
     console.log(err);
   } else {
+    let counter = 0;
     let films = JSON.parse(body).results;
     for (let result = 0; result < films.length; result++) {
-      for (let char = 0; char < films[result]['characters'].length; char++) {
-        if (films[result]['characters'][char] === 'https://swapi.co/api/people/18/') {
+      let characters = films[result]['characters'];
+      for (let j = 0; j < characters.length; j++) {
+        if (characters[j] === 'https://swapi.co/api/people/18/') {
           counter += 1;
         }
       }
     }
+    console.log(counter);
   }
-  console.log(counter);
 });
