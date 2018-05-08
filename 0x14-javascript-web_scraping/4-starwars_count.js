b65ee95cd45e875d0ca2b2d249e07ed3d1e49e47
stvngrcia <stevengarciaacuna@gmail.com>
2018-05-08 10:51:25 -0700

    parsing through an API counting the numeber of times a character appears

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+let request = require('request');
+let url = process.argv[2];
+let counter = 0;
+request(url, function (err, data, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    let films = JSON.parse(body)['results'];
+    for (let result = 0; result < films.length; result++) {
+      for (let char = 0; char < films[result]['characters'].length; char++) {
+        if (films[result]['characters'][char] === 'https://swapi.co/api/people/18/') {
+          counter += 1;
+        }
+      }
+    }
+  }
+  console.log(counter);
+});
