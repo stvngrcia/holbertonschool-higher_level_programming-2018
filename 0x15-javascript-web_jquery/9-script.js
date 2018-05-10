49b828b22475e8f5517f067af6ebf36f9f639c00
stvngrcia <stevengarciaacuna@gmail.com>
2018-05-10 12:46:13 -0700

    fetching data from an api to display the wind speed of city of san francisco

---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,8 @@
+$(function () {
+  let url = 'https://query.yahooapis.com/v1/public/yql?q=select%20wind%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22San%20Francisco%2C%20CA%22)&format=json';
+
+  $.get(url, function (data, status) {
+    let speed = data['query']['results']['channel']['wind']['speed'];
+    $('#sf_wind_speed').text(speed);
+  });
+});
